7199a14aabb661962dcd0b06151df755df3f4d15
Cerasus04 <cerasus1004@gmail.com>
2022-01-17 10:49:22 +0200

    footer

--- markup/components/nav-list/data/data.js
+++ markup/components/nav-list/data/data.js
@@ -1,12 +1,27 @@
 data = {
     nav: {
-        defaults: [
+        header: [
             {
                 'href': 'catalog.html',
                 'text': 'Каталог'
             }, {
                 'href': '#',
                 'text': 'О нас'
+            }, {
+                'href': '#',
+                'text': 'Контакты'
+            }
+        ],
+        footer: [
+            {
+                'href': 'catalog.html',
+                'text': 'Каталог'
+            }, {
+                'href': '#',
+                'text': 'О нас'
+            }, {
+                'href': '#',
+                'text': 'Магазины'
             }, {
                 'href': '#',
                 'text': 'Контакты'
